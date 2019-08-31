0d0f394a9a01916fbfc377c815ff3ee0190b2428
980f <andy.980f@gmail.com>
2019-08-30 21:46:48 -0500

    structurally sound but runs amok on reception

--- DFRobotDFPlayerMini.cpp
+++ DFRobotDFPlayerMini.cpp
@@ -1,4 +1,4 @@
-/*!
+/*!Heavily altered from:
    @file DFRobotDFPlayerMini.cpp
    @brief DFPlayer - An Arduino Mini MP3 Player From DFRobot
    @n Header file for DFRobot's DFPlayer
@@ -12,6 +12,8 @@
 */
 
 #include "DFRobotDFPlayerMini.h"
+
+const char *Packet::prefix = "\x7E\xFF\06";//{ 0x7E,  0xFF, 6}; //theoretically could dynamically switch to 4 but none of the examples do so
 
 #define DF_DEBUG Serial
 
@@ -31,11 +33,12 @@ bool DFRobotDFPlayerMini::sendCommand(uint8_t ccode, uint16_t param ) {
   return shipit();
 }
 
-bool DFRobotDFPlayerMini::sendCommand(uint8_t ccode, uint8_t param1 , uint8_t param2) {
+bool DFRobotDFPlayerMini::sendCommand(uint8_t opcode, uint8_t param1 , uint8_t param2) {
+  dbg("\nSC:", opcode, ',', param1, ',', param2);
   if (!ready) {  //if the ack mode is on wait until the last transmition
     return false;
   }
-  outgoing.fill(ccode, param1, param2);
+  outgoing.fill(opcode, param1, param2);
   return shipit();
 }
 
@@ -43,16 +46,18 @@ bool DFRobotDFPlayerMini::shipit() {
   auto pun = reinterpret_cast<uint8_t*>(&outgoing);
   auto len = sizeof(outgoing);
 
-  dbg(F("\nsending:"), BLOCK(pun, len, ' '));
+  dbg("\nsending:", BLOCK(pun, len, ' '));//the F() here took more bytes than it wrapped! Netted 32 bytes removing it.
 
+  _serial->write(Packet::prefix, 3);
   _serial->write(pun, len);
+  _serial->write(Packet::em);
 
-  responseExpected = 500;
+  responseOverdue = 500;
 
   switch (outgoing.opcode) {
     case 0x0C://reset
       ready = 2000; //or 1500 or 3000 depending upon who you believe.
-      responseExpected = 3000;
+      responseOverdue = 3000;
       break;
     case 0x09://change device
       ready = 200;//time to make filelist.
@@ -70,12 +75,31 @@ bool DFRobotDFPlayerMini::shipit() {
 
 void DFRobotDFPlayerMini::onMilli(Handler handler) {
   for (auto few = _serial->available(); few-- > 0;) { //ignoring bytes coming in while we are processing others in order to not hang if we fall behind on our processing.
-    if (r(_serial->read())) {
+    if (r(_serial->read())) {//true on well framed packet
       //check cs
-      //check if relevant to us
-      //if user request return true.
-      handler(r.incoming.opcode,r.incoming.param());
+      if (r.csok) {
+        auto opcode = r.incoming.opcode;
+        auto param = r.incoming.param();
+        //check if relevant to us
+        switch (opcode) {
+          case 0x3F:
+            ready = 200; //it needs more time after returning that before accepting another command, time to scan the disk.
+            break;
+          case 0x40://packet rejected
+            dbg("\nrejected", param);
+            break;
+        }
+        if(r.receptions<10)
+        handler(opcode, param);
+      } else {
+        //who do we report errors to?
+        dbg("\nchecksum error");
+      }
     }
+    r.prepare();//ready for next
+  }
+  if (responseOverdue) {
+    handler(0, MilliTick(responseOverdue));
   }
 }
 
@@ -85,83 +109,96 @@ void DFRobotDFPlayerMini::begin(Stream &stream, bool requestACK, bool doReset) {
 
   if (doReset) {
     reset();//sets timers that will block actions until responses have been noticed.
+  } else {
+    ready = 1234; //give the thing some time to power up.
   }
 
 }
 
 
-//bool DFRobotDFPlayerMini::parseStack() {
-//  uint8_t handleCommand = _received[Stack::Command];
-//  if (handleCommand == 0x41) { //handle the 0x41 ack feedback as a special case, in case the corruption of _handleCommand, _handleParameter, and _handleType.
-//    _isSending = false;
-//    return;
-//  }
-//
-//  _handleCommand = handleCommand;
-//  _handleParameter =  arrayToUint16(&_received[Stack::Parameter]);
-//
-//  switch (_handleCommand) {
-//    case 0x3D:
-//      handleMessage(Event::PlayFinished, _handleParameter);
-//      break;
-//    case 0x3F:
-//      if (_handleParameter & 0x01) {
-//        handleMessage(Event::USBOnline, _handleParameter);
-//      }
-//      else if (_handleParameter & 0x02) {
-//        handleMessage(Event::CardOnline, _handleParameter);
-//      }
-//      else if (_handleParameter & 0x03) {
-//        handleMessage(Event::CardUSBOnline, _handleParameter);
-//      }
-//      break;
-//    case 0x3A:
-//      if (_handleParameter & 0x01) {
-//        handleMessage(Event::USBInserted, _handleParameter);
-//      }
-//      else if (_handleParameter & 0x02) {
-//        handleMessage(Event::CardInserted, _handleParameter);
-//      }
-//      break;
-//    case 0x3B:
-//      if (_handleParameter & 0x01) {
-//        handleMessage(Event::USBRemoved, _handleParameter);
-//      }
-//      else if (_handleParameter & 0x02) {
-//        handleMessage(Event::CardRemoved, _handleParameter);
-//      }
-//      break;
-//    case 0x40:
-//      handleMessage(Event::Error, _handleParameter);
-//      break;
-//    case 0x3C:
-//    case 0x3E:
-//    case 0x42:
-//    case 0x43:
-//    case 0x44:
-//    case 0x45:
-//    case 0x46:
-//    case 0x47:
-//    case 0x48:
-//    case 0x49:
-//    case 0x4B:
-//    case 0x4C:
-//    case 0x4D:
-//    case 0x4E:
-//    case 0x4F:
-//      handleMessage(Event::FeedBack, _handleParameter);
-//      break;
-//    default:
-//      handleError(WrongStack);
-//      break;
-//  }
-//}
+void DFRobotDFPlayerMini::Receiver::prepare() {
+  sum = Packet::csinit ; //compiler should compute this and do a simple assignment of 0x105;
+  csok = false;
+  idx = Header;
+  complete = false;
+}
+
+void DFRobotDFPlayerMini::Receiver::accept(uint8_t bite) {
+  reinterpret_cast<uint8_t*>(&incoming)[idx] = bite;
+  sum += bite;
+  ++idx;
+}
+
+bool DFRobotDFPlayerMini::Receiver::verify(uint8_t bite) {
+  uint8_t cf =  Packet::prefix[idx];
+  if (cf == bite) {
+    ++idx;
+    return true;
+  } else {
+    ++framingErrors;
+    idx = Packet::prefix[0] == bite ? Version : Header;
+    return false;
+  }
+}
+
+bool DFRobotDFPlayerMini::Receiver::operator()(uint8_t bite) {
+  ++receptus;
+  switch (idx) {
+    case Header:
+    case Version:
+    case Length:
+      verify(bite);
+      break;
+    case Command:
+    case ACK:
+    case Parameter:
+    case ParameterLow:
+      accept(bite);
+      break;
+    case CheckSum:
+      sum += bite << 8;
+      ++idx;
+      break;
+    case CheckSumLow:
+      sum += bite;
+      csok = sum == 0;
+      ++idx;
+      break;
+    case End:
+      complete = Packet::em == bite; //but cs might be bad.
+      if (complete) {
+        ++receptions;
+      } else {
+        ++framingErrors;
+      }
+      break;
+    default:
+      prepare();
+      break;
+  }
+  return complete;
+}
+
+void Packet::fill(uint8_t opcode, uint8_t parmlow, uint8_t parmhigh) {
+
+  CsType sum = csinit;
+  sum += this-> opcode = opcode;
+  sum += ackPlease;
+  sum += this->parmhigh = parmhigh;
+  sum += this->parmlow = parmlow;
+  sum = -sum;
+  cshigh = sum >> 8;
+  cslow = sum;
+}
+
+#if DF_Include_Helpers
+//helper helpers go here
 
 bool DFRobotDFPlayerMini::readFileThing(uint8_t code, Medium device) {
   if (device == PriorChoice) {
     device = this->device;
   }
- 
+
   switch (device) {
     case Medium::U_DISK:
       break;
@@ -176,3 +213,5 @@ bool DFRobotDFPlayerMini::readFileThing(uint8_t code, Medium device) {
   }
   return sendCommand(code);
 }
+
+#endif
--- DFRobotDFPlayerMini.h
+++ DFRobotDFPlayerMini.h
@@ -1,7 +1,7 @@
 #pragma once
 //by us not defining the old header guard you can detect which version you have included.
 
-/*! Heaviy altered from:
+/*! Heavily altered from:
    @file DFRobotDFPlayerMini.h
    @brief DFPlayer - An Arduino Mini MP3 Player From DFRobot
    @n Header file for DFRobot's DFPlayer
@@ -13,18 +13,15 @@
    @version  V1.0.3
    @date  2016-12-07
 
-  980f comments:
-  'stack' is used for the communications packet, the thing that gets passed through a network stack.
-  I replaced #defines with enums, and then reworked function calls to use the enum's rather than any old byte.
-    Many of the enumerated values are never used.
-  I replaced *( buffer + offset) with buffer[offset].
-  I used the formal means of getting the correct type for use with millisecond timer.
-  I replaced delay(0) with a #define of YIELD to make it easier to search for these when I switch this to event driven rather than co-process. I have ESP8266's in hand.
 
 */
 
 #include "Arduino.h"  //helps some IDE's
-#include "index.h"
+#include "chainprinter.h" //todo: conditional on debug flag
+
+#ifndef DF_Include_Helpers
+#pragma warn("#define DF_Include_Helpers to a nonzero value to get convenience wrappers for commands.");
+#endif
 
 enum Equalizer {
   NORMAL = 0,
@@ -40,68 +37,51 @@ enum LoopMode {
   Random
 };
 
-/** parameter for command 09 */
+/** parameter for command 09, 0x32 1st byte */
 enum Medium : uint8_t { //#typed because we track which one we last requested
   PriorChoice = 255,
   U_DISK = 0,
   SDcard,  //SD symbol is taken by Arduino.h, caused weird coloring in arduino IDE
   AUX,
   SLEEP,
-  FLASH,
+  FLASH, //this guy's command tokens are missing in newer documents.
 };
 
 
+//these were statuses the original code generated
+//enum Event : uint8_t {
+//  TimeOut = 0, WrongStack, CardInserted, CardRemoved , CardOnline , PlayFinished, Error, USBInserted , USBRemoved, USBOnline, CardUSBOnline, FeedBack
+//};
 
-enum Event : uint8_t {
-  TimeOut = 0, WrongStack, CardInserted, CardRemoved , CardOnline , PlayFinished, Error, USBInserted , USBRemoved, USBOnline, CardUSBOnline, FeedBack
-};
-
+//0x42 2nd byte, 1st is Medium
 enum Issue : uint8_t {
   Busy = 1, Sleeping , SerialWrongStack , CheckSumNotMatch , FileIndexOut , FileMismatch , Advertise
 };
 
 //__attribute__ ((packed))
 struct Packet {
-  const uint8_t sm = 0x7E;
-  const uint8_t ver = 0xFF;
-  const uint8_t len = 6; //theoretically could dynamically switch to 4 but none of the examples do so
+  enum {  em = 0xEF , csinit = 0x105};
+  static const char *prefix;
   uint8_t opcode;//typeless so that user can send ones we don't define for them
   uint8_t ackPlease = 1; //not const as we want user to be able to run open loop
   uint8_t parmhigh;
   uint8_t parmlow;
   uint8_t cshigh;
   uint8_t cslow;
-  const uint8_t em = 0xEF;
 
   using CsType = uint16_t;
-  CsType csinit() const {
-    return  sm + ver + len ; //compiler should compute this and do a simple assignment of 0x105;
-  }
 
   uint16_t param() const {
     return parmhigh << 8 | parmlow;
   }
 
-  void fill(uint8_t opcode, uint8_t parmlow, uint8_t parmhigh) {
-    CsType sum = csinit();
-    sum += this-> opcode = opcode;
-    sum += ackPlease;
-    sum += this->parmhigh = parmhigh;
-    sum += this->parmlow = parmlow;
-    sum = -sum;
-    cshigh = sum >> 8;
-    cslow = sum;
-  }
+  void fill(uint8_t opcode, uint8_t parmlow, uint8_t parmhigh);
 
   void fill(uint8_t opcode, uint16_t parm = 0) {
     fill(opcode, parm, parm >> 8);
   }
 
-
-
-
 };
-
 
 #include "millievent.h"
 
@@ -113,54 +93,32 @@ class DFRobotDFPlayerMini {
     /**retain last selection for user requesting stats */
     Medium device = Medium::PriorChoice;//guaranteed to cause a message to be sent to host when a device is actively chosen.
 
-    MonoStable responseExpected;
-    //    Tick _timeOutDuration = 500;
+    MonoStable responseOverdue;//NYI: signal a timeout to handler.
 
     Packet outgoing;
     struct Receiver {
       /** named indexes into message. Messages are curiously referred to as a stack.*/
       enum Index {
-        Header = 0,
-        Version, Length , Command , ACK,
-        Parameter, ParameterLow,
-        CheckSum, CheckSumLow, End,
-        Allocation
+        Header = 0, Version, Length , Command , ACK, Parameter, ParameterLow, CheckSum, CheckSumLow, End, Allocation
       };
       Packet incoming;
       unsigned idx;
       Packet::CsType sum;
       bool csok;
       bool complete;
+      unsigned receptus = 0;
       unsigned framingErrors = 0;
+      unsigned receptions = 0;
 
       Receiver() {
         prepare();
       }
 
-      void prepare() {
-        sum = incoming.sm + incoming.ver + incoming.len ; //compiler should compute this and do a simple assignment of 0x105;
-        csok = false;
-        idx = Header;
-        complete = false;
-      }
+      void prepare();
 
-      void accept(uint8_t bite) {
-        reinterpret_cast<uint8_t*>(&incoming)[idx] = bite;
-        sum += bite;
-        ++idx;
-      }
+      void accept(uint8_t bite);
 
-      bool verify(uint8_t bite) {
-        uint8_t cf =  reinterpret_cast<uint8_t*>(&incoming)[idx];
-        if (cf == bite) {
-          ++idx;
-          return true;
-        } else {
-          ++framingErrors;
-          idx = incoming.sm == bite ? Version : Header;
-          return false;
-        }
-      }
+      bool verify(uint8_t bite);
 
       void check(uint8_t bite) {
         reinterpret_cast<uint8_t*>(&incoming)[idx] ^= bite;
@@ -168,44 +126,13 @@ class DFRobotDFPlayerMini {
       }
 
       /** call this with each byte received. When it returns true then immediatley interpret the contents then call prepare() */
-      bool operator()(uint8_t bite) {
-        switch (idx) {
-          case Header:
-          case Version:
-          case Length:
-          case Command:
-          case ACK:
-          case Parameter:
-          case ParameterLow:
-            accept(bite);
-            break;
-          case CheckSum:
-            sum += bite << 8;
-            ++idx;
-            break;
-          case CheckSumLow:
-            sum += bite;
-            csok = sum == 0;
-            ++idx;
-            break;
-          case End:
-            complete = incoming.em == bite; //but cs might be bad.
-            if (!complete) {
-              ++framingErrors;
-            }
-            break;
-          default:
-            prepare();
-            break;
-        }
-        return complete;
-      }
+      bool operator()(uint8_t bite);
     };
 
     Receiver r;
 
- bool shipit();
- 
+    bool shipit();
+
     bool waitingOnInit = false; //3F expected
     bool readFileThing(uint8_t code, Medium device);
   public:
@@ -222,19 +149,29 @@ class DFRobotDFPlayerMini {
       @returns whether it did it, which it won't if the interface is busy*/
     bool sendCommand(uint8_t ccode, uint16_t param = 0);
 
- /** if you call directly you are on your own for the most part.
-      @returns whether it did it, which it won't if the interface is busy*/
+    /** if you call directly you are on your own for the most part.
+         @returns whether it did it, which it won't if the interface is busy*/
     bool sendCommand(uint8_t ccode, uint8_t param1 , uint8_t param2);
-
 
     /** setup call. @param requestACK is whether you desire to get acknowledgement messages for your commands. @param doReset restarts the device, it will not be ready for a few seconds */
     void begin(Stream& stream, bool requestACK = true, bool doReset = true);
 
     /** ACK means 'please send response to all commands, not just data queries' */
-    void ACK(bool on) {
+    void ACK(bool on) { //this is not a helper, it sets a local protocol option.
       outgoing.ackPlease = on;//bool conveniently matches command values
     }
 
+    bool reset() {//used by begin
+      return sendCommand(0x0C);
+    }
+
+    void showstate(ChainPrinter &cp) {
+      cp("Ready:", bool(ready), " ", MilliTick(ready), ", R:", r.receptions, ", E:", r.framingErrors, ", B:", r.receptus);
+      cp("Sent:", HEXLY(outgoing.opcode), "@", outgoing.param());
+      cp("Recv:", HEXLY(r.incoming.opcode), "@", r.incoming.param());
+    }
+
+#if DF_Include_Helpers
     bool outputDevice(Medium device) {
       if (this->device != device) {
         this->device = device;
@@ -244,123 +181,114 @@ class DFRobotDFPlayerMini {
       }
     }
 
-    void Loop(bool on = true) {
-      sendCommand(0x19, on ? 0 : 1); //complement of most
+    bool Loop(bool on = true) {
+      return sendCommand(0x19, on ? 0 : 1); //complement of most
     }
 
-    void DAC(bool on) {
-      sendCommand(0x1A, on ? 0 : 1); //complement of most
+    bool DAC(bool on) {
+      return sendCommand(0x1A, on ? 0 : 1); //complement of most
     }
 
-    void LoopAll(bool on = true) {
-      sendCommand(0x11, on);
+    bool LoopAll(bool on = true) {
+      return sendCommand(0x11, on);
     }
 
-
-    void play(unsigned fileNumber = 1) {
-      sendCommand(0x03, fileNumber);
+    bool play(unsigned fileNumber = 1) {
+      return sendCommand(0x03, fileNumber);
     }
 
-    void playFolder(uint8_t folderNumber, uint8_t fileNumber) {
-      sendCommand(0x0F, folderNumber, fileNumber);
-    }
-    void loop(LoopMode play, bool andBegin = true);
-
-
-    void loop(LoopMode play) {
-      sendCommand(0x08, play);
+    bool playFolder(uint8_t folderNumber, uint8_t fileNumber) {
+      return sendCommand(0x0F, folderNumber, fileNumber);
     }
 
-
-    void playLargeFolder(uint8_t folderNumber, uint16_t fileNumber) {
-      sendCommand(0x14, (((uint16_t)folderNumber) << 12) | fileNumber);
+    bool loop(LoopMode play) {
+      return sendCommand(0x08, play);
     }
 
-    void loopFolder(unsigned folderNumber) {
-      sendCommand(0x17, folderNumber);
+    bool playLargeFolder(uint8_t folderNumber, uint16_t fileNumber) {
+      return sendCommand(0x14, (((uint16_t)folderNumber) << 12) | fileNumber);
     }
 
-//play from a folder named "MP3"
-    void playMp3Folder(unsigned fileNumber) {
-      sendCommand(0x12, fileNumber);
+    bool loopFolder(unsigned folderNumber) {
+      return sendCommand(0x17, folderNumber);
     }
 
-    void randomAll() {
-      sendCommand(0x18);
+    //play from a folder named "MP3"
+    bool playMp3Folder(unsigned fileNumber) {
+      return sendCommand(0x12, fileNumber);
     }
 
+    bool randomAll() {
+      return sendCommand(0x18);
+    }
 
     /** advertise interrupts loop play to play given item, returns to loop play,  pass a zero to return sooner or cancel one that hasn't started */
-    void advertise(unsigned fileNumber) {
-      sendCommand(fileNumber ? 0x13 : 0x15, fileNumber);
+    bool advertise(unsigned fileNumber) {
+      return sendCommand(fileNumber ? 0x13 : 0x15, fileNumber);
     }
 
     //1..31
-    void volume(uint8_t volume) {
-      sendCommand(0x06, volume);
+    bool volume(uint8_t volume) {
+      return sendCommand(0x06, volume);
     }
 
-    void outputSetting(bool enable, uint8_t gain) {
-      sendCommand(0x10, enable, gain);
+    bool outputSetting(bool enable, uint8_t gain) {
+      return sendCommand(0x10, enable, gain);
+    }
+
+    bool EQ(Equalizer eq) {
+      return sendCommand(0x07, eq);
+    }
+
+    bool next() {
+      return sendCommand(0x01);
+    }
+
+    bool previous() {
+      return sendCommand(0x02);
+    }
+
+    bool volumeUp() {
+      return sendCommand(0x04);
+    }
+
+    bool volumeDown() {
+      return sendCommand(0x05);
+    }
+
+    bool stop() {
+      return sendCommand(0x16);
+    }
+
+    bool sleep() {
+      return sendCommand(0x0A);
     }
 
 
-    void EQ(Equalizer eq) {
-      sendCommand(0x07, eq);
+    bool start() {
+      return sendCommand(0x0D);
     }
 
-    void next() {
-      sendCommand(0x01);
+    bool pause() {
+      return sendCommand(0x0E);
     }
 
-    void previous() {
-      sendCommand(0x02);
+    bool readState() {
+      return sendCommand(0x42);
     }
 
-    void volumeUp() {
-      sendCommand(0x04);
+    bool readVolume() {
+      return sendCommand(0x43);
     }
 
-    void volumeDown() {
-      sendCommand(0x05);
+    bool readEQ() {
+      return sendCommand(0x44);
     }
 
-    void stop() {
-      sendCommand(0x16);
-    }
-
-    void sleep() {
-      sendCommand(0x0A);
-    }
-
-    void reset() {
-      sendCommand(0x0C);
-    }
-
-    void start() {
-      sendCommand(0x0D);
-    }
-
-    void pause() {
-      sendCommand(0x0E);
-    }
-
-    int readState() {
-      sendCommand(0x42);
-    }
-
-    int readVolume() {
-      sendCommand(0x43);
-    }
-
-    int readEQ() {
-      sendCommand(0x44);
-    }
-
-    int readFileCounts(Medium device = PriorChoice) {
+    bool readFileCounts(Medium device = PriorChoice) {
       return readFileThing(0x47, device);
     }
-    int readCurrentFileNumber(Medium device = PriorChoice) {
+    bool readCurrentFileNumber(Medium device = PriorChoice) {
       return readFileThing(0x4B, device);
     }
 
@@ -368,7 +296,9 @@ class DFRobotDFPlayerMini {
       return sendCommand(0x4E, folderNumber);
     }
 
-    int readFolderCounts() {
-      sendCommand(0x4F);
+    bool readFolderCounts() {
+      return sendCommand(0x4F);
     }
+#endif
+
 };
